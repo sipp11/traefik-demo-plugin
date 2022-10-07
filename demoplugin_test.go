f41033a155e0a478dcbd09351cc7b048e121fc38
sipp11 <sipp11@zzyzx.co>
2022-10-07 11:02:38 +0700

    Initial commit

---
+++ demoplugin_test.go
@@ -0,0 +1,49 @@
+package demoplugin_test
+
+import (
+	"context"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	demoplugin "github.com/sipp11/traefik-demo-plugin"
+)
+
+func TestDemo(t *testing.T) {
+	cfg := demoplugin.CreateConfig()
+	cfg.Headers["X-Host"] = "[[.Host]]"
+	cfg.Headers["X-Method"] = "[[.Method]]"
+	cfg.Headers["X-URL"] = "[[.URL]]"
+	cfg.Headers["X-URL"] = "[[.URL]]"
+	cfg.Headers["X-Demo-One"] = "test"
+
+	ctx := context.Background()
+	next := http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {})
+
+	handler, err := demoplugin.New(ctx, next, cfg, "demo-plugin")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	recorder := httptest.NewRecorder()
+
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, "http://localhost", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	handler.ServeHTTP(recorder, req)
+
+	assertHeader(t, req, "X-Host", "localhost")
+	assertHeader(t, req, "X-URL", "http://localhost")
+	assertHeader(t, req, "X-Method", "GET")
+	assertHeader(t, req, "X-Demo-One", "test")
+}
+
+func assertHeader(t *testing.T, req *http.Request, key, expected string) {
+	t.Helper()
+
+	if req.Header.Get(key) != expected {
+		t.Errorf("invalid header value: %s", req.Header.Get(key))
+	}
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/sipp11/traefik-demo-plugin
+
+go 1.19
